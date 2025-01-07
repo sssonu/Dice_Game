10fbf9604889795280ce04edae2b9c1cbe61e932
Sonu Garg <144627790+sssonu@users.noreply.github.com>
2025-01-07 18:03:26 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -2,14 +2,14 @@
 var r1 =Math.random();
 r1=Math.floor((r1*6))+1;
 
-var SrcPathImg1="images/"+"dice"+r1+".png";
+var SrcPathImg1="dice"+r1+".png";
 
 var selectImg1=document.querySelector(".img1");
 selectImg1.setAttribute("src", SrcPathImg1);
 
 //For second dice
 var r2=Math.floor((Math.random()*6)+1);
-var ScrPathImg2="images/"+"dice"+r2+".png";
+var ScrPathImg2="dice"+r2+".png";
 var selectImg2=document.querySelectorAll("img")[1];
 selectImg2.setAttribute("src",ScrPathImg2);
 
