798ec00bb59f0784bb813a8e0016131f741f1a57
Sonu Garg <144627790+sssonu@users.noreply.github.com>
2025-01-07 17:56:43 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-//var r1 =Math.random();
-// r1=Math.floor((r1*6))+1;
-
-// var randomDiceImg1="dice"+r1+".png";
-
-// var img1Path="images/"+randomDiceImg1;
-
-// var selectImg1=document.querySelectorAll("img")[0];
-// selectImg1.setAttribute("src", img1Path);
